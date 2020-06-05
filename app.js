de490037685fd97e47b507ed0e0653084a47fac2
dominggo1999 <arnolddominggo19@gmail.com>
2020-06-06 04:08:21 +0700

    add preloader

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const nav = document.querySelector('.navbar');
-
+const preloader = document.querySelector('.preloader');
 window.addEventListener("scroll",e=>{
 	nav.classList.toggle('sticky',pageYOffset > 0);
 })
+
+// Smooth Scrolling
+let scroll = new SmoothScroll('a[href*="#"]', 
+	{
+		speed: 1500,
+		speedAsDuration: true
+	}, 
+	{
+		easing: 'easeInCubic'
+	}
+);
+
+
+// Preloader
+setTimeout(()=>{
+	preloader.classList.add("gone");
+},5000);
